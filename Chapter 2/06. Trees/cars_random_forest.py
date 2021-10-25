4c22603108e24ab2fed464c6398e8f8cb8027b74
pvillegasmartin <pvillegasmartin@gmail.com>
2021-10-25 15:05:19 +0200

    Random forest (PCA)

--- Chapter 2/06. Trees/cars_random_forest.py
+++ Chapter 2/06. Trees/cars_random_forest.py
@@ -4,6 +4,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 from sklearn.metrics import plot_confusion_matrix, accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
 
 # Load dataset
 attributes = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety','target']
@@ -15,6 +17,21 @@ X, y = df.iloc[:,:-1], df.target
 X.persons = X.persons.replace('more',5)
 X.doors = X.doors.replace('5more',5)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
+# PCA
+
+pca = PCA()
+classifier = RandomForestClassifier()
+pipe = make_pipeline(pca, classifier)
+#print(pipe.get_params().keys())
+parameters = {'pca__n_components':[2,4,5,6,7,8,10],'randomforestclassifier__criterion':('gini', 'entropy')}
+clf = GridSearchCV(pipe, parameters)
+clf.fit(X_train, y_train)
+best_parameters = clf.best_params_ # we see best parameters are criterion='entropy', max_depth=None, min_samples_split=2, n_estimators=100, PCA: n_components=6
+print(best_parameters)
+predictions = clf.predict(X_test)
+acc_score = accuracy_score(y_test, predictions)
+print(acc_score)
 
 # Model Selection
 
@@ -31,7 +48,7 @@ print(best_parameters)
 classifier = RandomForestClassifier(criterion='entropy', max_depth=None, min_samples_split=2, n_estimators=100)
 cv = cross_validate(classifier, X_train, y_train, cv=5)
 print(cv['test_score'].mean()) # accuracy mean of 0.9660882685778951
-'''
+
 
 clf = RandomForestClassifier(criterion='entropy', max_depth=None, min_samples_split=2, n_estimators=100)
 clf = clf.fit(X_train, y_train)
@@ -43,4 +60,5 @@ predictions = clf.predict(X_test)
 plot_confusion_matrix(clf, X_test, y_test, display_labels=["unacc", "acc", "good", "vgood"])
 plt.show()
 acc_score = accuracy_score(y_test, predictions)
-print(acc_score) # accuracy = 0.9749518304431599
+print(acc_score) # accuracy before PCA = 0.9749518304431599
+'''
