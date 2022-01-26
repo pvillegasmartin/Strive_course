8e2272fc307ce52288b904a03eacd95816ceea9e
pvillegasmartin <pvillegasmartin@gmail.com>
2022-01-26 14:50:24 +0100

    Spark

---
+++ Chapter 4-SE/Spark/test.py
@@ -0,0 +1,10 @@
+import findspark
+findspark.init()
+
+import pyspark # only run after findspark.init()
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.sql('''select 'spark' as hello ''')
+df.show()
